58e016f8896bb32dfb307cde3d8c555954addbe1
Keith Estrella <keithestrella@gmail.com>
2025-05-23 03:04:05 -0400

    Add work section

---
+++ src/form/Work.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+
+function Work(){
+    const generateID = () => {
+        return Date.now() + Math.random();
+    }
+    
+    const [formData, setFormData] = useState([{id: generateID(), workCompany: "", workTitle: "", workDesc: "", workStartDate:"", workEndDate:""}]);
+
+    const handleChange = (index, e) => {
+        const updatedForm = [...formData];
+        updatedForm[index][e.target.name] = e.target.value;
+        setFormData(updatedForm);
+    };
+
+    const addWork = () => {
+        setFormData([...formData, {id: generateID(), workCompany: "", workTitle: "", workDesc: "", workStartDate:"", workEndDate:""}]);
+    };
+
+    const removeWork = (id) => {
+        const updatedForm = formData.filter(work => work.id !== id);
+        setFormData(updatedForm);
+    };
+
+    return (
+        <>
+        <div id="form-work">
+            <h1 className="form-header">Work / Experience</h1>
+            {formData.map((work,index) => (
+                <div key={work.id}>
+                    <h3 className="work">Work {index+1}</h3>
+                    <div className="form-info-input">
+                        <label htmlFor="workCompany">Company Name: </label>
+                        <input type="text" id="workCompany" name="workCompany" value={work.workCompany} onChange={(e) => handleChange(index,e)} required />
+                    </div>
+                    <div className="form-info-input">
+                        <label htmlFor="workTitle">Title of Position: </label>
+                        <input type="text" id="workTitle" name="workTitle" value={work.workTitle} onChange={(e) => handleChange(index,e)} required />
+                    </div>
+                    <div className="form-info-input">
+                        <label htmlFor="workDesc">Position Description: </label>
+                        <textarea id="workDesc" name="workDesc" value={work.workDesc} onChange={(e) => handleChange(index,e)} required />
+                    </div>
+                    <div className="form-info-input">
+                        <label htmlFor="workStartDate">Start Date: </label>
+                        <input type="date" id="workStartDate" name="workStartDate" value={work.workStartDate} onChange={(e) => handleChange(index,e)} required />
+                    </div>
+                    <div className="form-info-input">
+                        <label htmlFor="workEndDate">End Date: </label>
+                        <input type="date" id="workEndDate" name="workEndDate" value={work.workEndDate} onChange={(e) => handleChange(index,e)} required />
+                    </div>
+                    <button type="button" onClick={() => removeWork(work.id)}>Remove</button>
+                </div>
+            ))}
+            <button type="button" onClick={addWork}>Add Work</button>
+        </div>
+        </>
+    )
+}
+
+export default Work
