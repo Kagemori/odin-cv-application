9afee8b1820490992e3b61e2e16e48257b1a2a39
Keith Estrella <keithestrella@gmail.com>
2025-05-31 02:41:17 -0400

    Add useState to Information section and prepare for ResumePreview

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './index.css'
 import Information from './form/Information.jsx'
 import Education from './form/Education.jsx'
 import Work from './form/Work.jsx'
+import ResumePreview from './ResumePreview.jsx'
 import { useState } from "react";
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
     return Date.now() + Math.random();
   }
 
+  const [formInfoData, setFormInfoData] = useState([{id: generateID(), infoName: "", infoEmail: "", infoPhone: ""}])
   const [formSchoolData, setFormSchoolData] = useState([{id: generateID(), schoolname: "", schoolstudy: "", schoolstartdate: "", schoolenddate:""}]);
   const [formWorkData, setFormWorkData] = useState([{id: generateID(), workCompany: "", workTitle: "", workDesc: "", workStartDate:"", workEndDate:""}]);
 
@@ -18,10 +20,18 @@ const App = () => {
       <div id="container"> 
         <h1 id="form-header">Resume Builder</h1>
         <form id="form-container">
-          <Information />
+          <Information data={formInfoData} setData={setFormInfoData}/>
           <Education data={formSchoolData} setData={setFormSchoolData}/>
           <Work data={formWorkData} setData={setFormWorkData}/>
         </form>
+      </div>
+      <div id="preview-container">
+        <h1 id="preview-header">Resume Preview</h1>
+        <div id="preview-page">
+          <div id="preview-div">
+            <ResumePreview infoData={formInfoData} schoolData={formSchoolData} workData={formWorkData}/>
+          </div>
+        </div>
       </div>
     </StrictMode>
   );
--- src/form/Information.jsx
+++ src/form/Information.jsx
@@ -1,20 +1,19 @@
-function Information(){
-
+const Information = ({data,setData}) => {
     return (
         <>
         <div id="form-information">
             <h1 className="form-header">Information</h1>
             <div className="form-info-input">
                 <label htmlFor="name">Name: </label>
-                <input type="text" id="name" name="name" defaultValue="John Doe" required/>
+                <input type="text" id="name" name="name" value={data.infoName} onChange={(e) => setData({...data, infoName: e.target.value})} required/>
             </div>
             <div className="form-info-input">
                 <label htmlFor="email">E-mail: </label>
-                <input type="email" id="email" name="email" defaultValue="johndoe@co.com" required />
+                <input type="email" id="email" name="email" value={data.infoEmail} onChange={(e) => setData({...data, infoEmail: e.target.value})} required />
             </div>
             <div className="form-info-input">
                 <label htmlFor="phone">Phone Number: </label>
-                <input type="tel" id="phone" name="phone" defaultValue="555-555-5555" required />
+                <input type="tel" id="phone" name="phone" value={data.infoPhone} onChange={(e) => setData({...data, infoPhone: e.target.value})} required />
             </div>
         </div>
         </>
